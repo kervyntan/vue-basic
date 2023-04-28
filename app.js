65ac70fa29f91fcaec7eb7ebb9149e0c50741de0
Kervyn <kervyntan@gmail.com>
2023-04-28 15:02:55 +0800

    updated comments

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ console.log("Test");
 
 // Lesson 2
 const app = Vue.createApp({
-    // can render a manual template with html here
-    template : "<h2> This is a test template <h2>"
+    // store data and functions
+
+    // can render a manual template with html here but gets messy
+    // template : "<h2> This is a test template <h2>"
 });
 
 // mount the element with id app in html file
