f12fbdb2b32ce0efdc7e46481b7c1d36cbfe51cd
Kervyn <kervyntan@gmail.com>
2023-04-28 15:05:59 +0800

    passing in variables from data() into index.html templateq

--- app.js
+++ app.js
@@ -4,6 +4,15 @@ console.log("Test");
 // Lesson 2
 const app = Vue.createApp({
     // store data and functions
+    data() {
+        // data function
+        // returns an object
+        return {
+            title : "Vue Basics",
+            part : 2,
+            version : 1.0
+        }
+    }
 
     // can render a manual template with html here but gets messy
     // template : "<h2> This is a test template <h2>"
