1c692fd3890710715499a5f43056f02af0e9c531
Kervyn <kervyntan@gmail.com>
2023-05-02 09:53:57 +0800

    Updating current counter value and changing display message

--- vue-basic/src/components/Counter.vue
+++ vue-basic/src/components/Counter.vue
@@ -1,6 +1,7 @@
 <template>
 <div class="counter"> {{ count }} </div>
 <button class="btn" @click="increment">Increment value</button>
+<p> {{  message }} </p>
 </template>
 
 <script>
@@ -8,7 +9,8 @@ import { watch } from 'vue';
 export default {
     data () {
         return {
-            count : 0
+            count : 0,
+            message : ""
         }
     },
     methods : {
@@ -20,6 +22,13 @@ export default {
         count: function(value) {
             // If "pageData" ever changes, then we will console log its new value.
             console.log(value);
+            if (value < 10) {
+                // need to use this to point to the correct instance
+                this.message = `You have clicked this less than ${value} times`;
+                // reference the value of count using the argument passed into the above function
+            } else {
+                this.message = `You have clicked this far too many times, ${value} times to be exact`;
+            }
         }
     }
 }
